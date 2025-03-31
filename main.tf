b6c150de484fe8e0a3fabc1ba2290ca66923ebf7
Muralisanku <sankumurali4a6@gmail.com>
2025-03-31 18:14:17 +0530

    mssing

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "roboshop" {
-  source = "../terrafrom-aws-vpc"
+  source = "../terraform-aws-vpc"
   project_name = var.project_name
   environment = var.environment
   common_tags = var.common_tags
